617b32cd9604af3835b6fdcd816527d921074afd
AdalidDuran <adalid.duran@outlook.com>
2025-09-22 21:53:29 -0600

    OMS-3 Database initial configuration

---
+++ OperationManagementService/Models/Transaction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OperationManagementService.Models;
+
+public partial class Transaction
+{
+    public int TransactionId { get; set; }
+
+    public int? OperationId { get; set; }
+
+    public Guid? UserId { get; set; }
+
+    public string TransactionName { get; set; } = null!;
+
+    public decimal Amount { get; set; }
+
+    public DateTime TransactionDate { get; set; }
+
+    public bool? IsDeleted { get; set; }
+
+    public virtual OperationLog? Operation { get; set; }
+
+    public virtual User? User { get; set; }
+}
