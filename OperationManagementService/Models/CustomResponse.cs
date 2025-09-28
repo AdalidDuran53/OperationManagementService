e9c42a6025fb14f7f8058bc0d4b0675ba0093890
AdalidDuran <adalid.duran@outlook.com>
2025-09-27 19:54:05 -0600

    OMS-11 session log method initial implementation

---
+++ OperationManagementService/Models/CustomResponse.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OperationManagementService.OperationExceptions;
+using System;
+using System.Collections.Generic;
+
+namespace OperationManagementService.Models;
+
+public class CustomResponse : ActionResult
+{
+    public string Message { get; }
+    public Guid UserId { get; }
+    public object Data { get; }
+
+    public CustomResponse(string message, Guid userId, object data = null)
+    {
+        this.Message = message;
+        this.UserId = userId;
+        this.Data = data;
+    }
+
+    public override async Task ExecuteResultAsync(ActionContext context)
+    {
+        var response = new
+        {
+            success = true,
+            message = Message,
+            data = Data
+        };
+
+        var objectResult = new ObjectResult(response)
+        {
+            StatusCode = StatusCodes.Status200OK
+        };
+
+        await objectResult.ExecuteResultAsync(context);
+    }
+}
