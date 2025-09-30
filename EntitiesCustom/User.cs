f268c119dd68a81e3bef40d45021e10878081085
AdalidDuran <adalid.duran@outlook.com>
2025-09-30 12:27:50 -0600

    OMS-16 add update user method

--- EntitiesCustom/User.cs
+++ EntitiesCustom/User.cs
@@ -15,7 +15,6 @@ public partial class User: IValidation
         this.PasswordHash = password;
         this.PasswordSalst = salst;
     }
-
     public Guid UserId { get; set; }
 
     public string? UserName { get; set; }
@@ -24,10 +23,6 @@ public partial class User: IValidation
     public string PasswordSalst { get; set; } = null!;
 
     public bool? IsDeleted { get; set; }
-
-    public virtual ICollection<SessionLog> SessionLogs { get; set; } = new List<SessionLog>();
-
-    public virtual ICollection<Transaction> Transactions { get; set; } = new List<Transaction>();
 
     // Validate the user object
     public string Validate(string operationExceptionCode)
@@ -38,7 +33,7 @@ public partial class User: IValidation
             operationExceptionCode = "OMS-USERNAME-ERROR";
         else if (String.IsNullOrEmpty(this.PasswordHash))
             operationExceptionCode = "OMS-PASSWORD-ERROR";
-        else if (this.PasswordHash.Length > 256)
+        else if (this.PasswordHash.Length > 50)
             operationExceptionCode = "OMS-PASSWORD-ERROR";
 
         return operationExceptionCode;
--- OperationManagementService/Business/UserFunctionality.cs
+++ OperationManagementService/Business/UserFunctionality.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using OperationExceptions;
+using System.ComponentModel.DataAnnotations;
 
 namespace OperationManagementService.Business
 {
@@ -114,6 +115,69 @@ namespace OperationManagementService.Business
 
                     // return the result
                     return new CustomResponse(statusCode: StatusCodes.Status200OK, message: "Deleted user successfully.", userId: user.UserId, sessionId: sessionId);
+                }
+            }
+            catch (Exception ex)
+            {
+                // if the exception is an OperationException, rethrow it
+                if (ex is OperationException)
+                    throw ex;
+                // otherwise, throw a general error
+                var exception = this._errorService.GetError("OMS-GENERAL-ERROR");
+                throw new OperationException(errorCode: exception.Code, message: exception.Message, details: exception.Details, sessionId: sessionId);
+            }
+        }
+
+        public async Task<CustomResponse> UpdateUser(Guid userId, Guid sessionId, string currentPassword, string newPassword, string userName)
+        {
+            try
+            {
+                using (var context = new Models.OperationContext())
+                {
+                    // find the user by user name
+                    var user = await context.Users
+                    .FirstOrDefaultAsync(u => u.UserId == userId && u.IsDeleted == false);
+                    // load the session logs for the user
+                   var SessionLogs = await context.SessionLogs.Where(s => s.UserId == userId && s.EndSession == null).ToListAsync();
+                    // if the user is not found, the session id does not match or the current password does not match, throw an error
+                    if (user == null || !SessionLogs.Any(s => s.SessionId == sessionId) || !VerifyPassword(currentPassword, user.PasswordHash, user.PasswordSalst))
+                    {
+                        var exception = this._errorService.GetError("OMS-SESSION-ERROR");
+                        throw new OperationException(errorCode: exception.Code, message: exception.Message, details: exception.Details, new Guid());
+                    }
+                    else
+                    {
+                        // if new password is provided, hash it and update the password
+                        if (!String.IsNullOrEmpty(newPassword))
+                        {
+                            // hash the new password
+                            var pass = HashPassword(newPassword);
+                            user.PasswordHash = pass.Hash;
+                            user.PasswordSalst = pass.Salt;
+                        }
+                        // if user name is provided, update the user name
+                        if (!String.IsNullOrEmpty(userName))
+                        {
+                            // check for duplicate user names
+                            var isInvalidUserName = await context.Users.AnyAsync(s => s.UserName.Equals(userName) && s.UserId != userId);
+                            if (isInvalidUserName)
+                            {
+                                // if the user name already exists, throw an error
+                                var exception = this._errorService.GetError("OMS-USERNAME-ERROR");
+                                throw new OperationException(errorCode: exception.Code, message: exception.Message, details: exception.Details, new Guid());
+                            }
+                            user.UserName = userName;
+                        }
+                        // map the user object to custom user model for validation
+                        var updatedUser = Mapster.TypeAdapter.Adapt<User>(user);
+                        this.ValidateModel(updatedUser);
+                        // update the user
+                        context.Users.Update(user);
+                        await context.SaveChangesAsync();
+                        // return the result
+                        return new CustomResponse(statusCode: StatusCodes.Status200OK, message: "Updated user successfully.", userId: user.UserId, sessionId: sessionId);
+                    }
+
                 }
             }
             catch (Exception ex)
