dc477dba7fb60367722eb8115bb4d99c76cf9af8
AdalidDuran <adalid.duran@outlook.com>
2025-10-02 15:20:25 -0600

    OMS-19 add transactions methods

---
+++ OperationManagementService/Controllers.Implementation/TransactionApi.cs
@@ -0,0 +1,117 @@
+﻿using EntitiesCustom;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using OperationExceptions;
+using OperationManagementService.Business;
+using OperationManagementService.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace OperationManagementService.Controllers.Implementation
+{
+    [ApiVersion("0.1")]
+    [ApiController]
+    [Authorize]
+    public class TransactionApi : TransactionControllerBase
+    {
+
+        private readonly UserFunctionality _userFunctionality;
+        private readonly ServiceBaseFunctionality _serviceBaseFunctionality;
+        private readonly ErrorServiceModel _errorService = new ErrorServiceModel();
+        public TransactionApi(UserFunctionality userFunctionality, ServiceBaseFunctionality serviceBaseFunctionality)
+        {
+            _userFunctionality = userFunctionality;
+            _serviceBaseFunctionality = serviceBaseFunctionality;
+            _errorService = new ErrorServiceModel();
+        }
+
+        [HttpPost]
+        [Route("~/{version::apiVersion}/Transactions/AddTransaction")]
+        public override async Task<IActionResult> AddTransaction([FromRoute, RegularExpression("^(?<major>[0-9]+).(?<major>[0-9]+)$"), Required] string version, [Required] Guid userId, [Required] Guid sessionId)
+        {
+            // Log the request
+            Dictionary<string, object> request = new Dictionary<string, object> { { "CreateAddTransactionRequest", new object[] { "version: " + version, "userId: " + userId, "sessionId: " + sessionId } } };
+            try
+            {
+                // return the result
+                return Ok("ok");
+            }
+            catch (Exception ex)
+            {
+                // Log the exception
+                Dictionary<string, object> response = new Dictionary<string, object> { { "ErrorCreateAddTransactionResponse", ex } };
+                _serviceBaseFunctionality.LogOperation(request, response);
+                // if the exception is an OperationException, return a bad request with the error details
+                OperationException excep = ((OperationException)ex);
+                return this.BadRequest(new { StatusCode = StatusCodes.Status400BadRequest, code = excep.ErrorCode, message = excep.Message, details = excep.Details});
+            }
+        }
+
+        [HttpPut]
+        [Route("~/{version::apiVersion}/Transactions/UpdateTransaction")]
+        public override async Task<IActionResult> UpdateTransaction([FromRoute, RegularExpression("^(?<major>[0-9]+).(?<major>[0-9]+)$"), Required] string version, [Required] Guid userId, [Required] Guid sessionId)
+        {
+            // Log the request
+            Dictionary<string, object> request = new Dictionary<string, object> { { "UpdateTransactionRequest", new object[] { "version: " + version, "userId: " + userId, "sessionId: " + sessionId } } };
+            try
+            {
+                // return the result
+                return Ok("ok");
+            }
+            catch (Exception ex)
+            {
+                // Log the exception
+                Dictionary<string, object> response = new Dictionary<string, object> { { "ErrorUpdateTransactionResponse", ex } };
+                _serviceBaseFunctionality.LogOperation(request, response);
+                // if the exception is an OperationException, return a bad request with the error details
+                OperationException excep = ((OperationException)ex);
+                return this.BadRequest(new { StatusCode =StatusCodes.Status400BadRequest, code = excep.ErrorCode, message = excep.Message, details = excep.Details });
+            }
+        }
+
+
+        [HttpDelete]
+        [Route("~/{version::apiVersion}/Transactions/DeleteTransaction")]
+        public override async Task<IActionResult> DeleteTransaction([FromRoute, RegularExpression("^(?<major>[0-9]+).(?<major>[0-9]+)$"), Required] string version, [Required] Guid userId, [Required] Guid sessionId)
+        {
+            // Log the request
+            Dictionary<string, object> request = new Dictionary<string, object> { { "DeleteTransactionRequest", new object[] { "version: " + version, "userId: " + userId, "sessionId: " + sessionId } } };
+            try
+            {
+                // return the result
+                return Ok("ok");
+            }
+            catch (Exception ex)
+            {
+                // Log the exception
+                Dictionary<string, object> response = new Dictionary<string, object> { { "ErrorDeleteTransactionResponse", ex } };
+                _serviceBaseFunctionality.LogOperation(request, response, sessionId);
+                // if the exception is an OperationException, return a bad request with the error details
+                OperationException excep = ((OperationException)ex);
+                return this.BadRequest(new { StatusCode = StatusCodes.Status400BadRequest, code = excep.ErrorCode, message = excep.Message, details = excep.Details });
+            }
+        }
+
+        [HttpGet]
+        [Route("~/{version::apiVersion}/Transactions/GetTransactions")]
+        public override async Task<IActionResult> GetTransactions([FromRoute][Required][RegularExpression("^(?<major>[0-9]+).(?<major>[0-9]+)$")] string version, [Required] Guid userId, [Required] Guid sessionId)
+        {
+            // Log the request
+            Dictionary<string, object> request = new Dictionary<string, object> { { "GetTransactionsRequest", new object[] { "version: " + version, "userId: " + userId, "sessionId: " + sessionId } } };
+            try
+            {
+                // return the result
+                return Ok("ok");
+            }
+            catch (Exception ex)
+            {
+                // Log the exception
+                Dictionary<string, object> response = new Dictionary<string, object> { { "ErrorGetTransactionsResponse", ex } };
+                _serviceBaseFunctionality.LogOperation(request, response, sessionId);
+                // if the exception is an OperationException, return a bad request with the error details
+                OperationException excep = ((OperationException)ex);
+                return this.BadRequest(new { StatusCode = StatusCodes.Status400BadRequest, code = excep.ErrorCode, message = excep.Message, details = excep.Details });
+            }
+        }
+    }
+}
---
+++ OperationManagementService/Controllers/TransactionControllerBase.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
+using Swashbuckle.AspNetCore.Annotations;
+using System.ComponentModel.DataAnnotations;
+
+namespace OperationManagementService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public abstract class TransactionControllerBase : ControllerBase
+    {
+
+        [HttpPost]
+        [EnableRateLimiting("IpPolicy")]
+        [Route("~/{version}/Transactions/")]
+        [SwaggerResponse(statusCode: 200, type: typeof(ActionResult), description: "Ok")]
+        [SwaggerResponse(statusCode: 400, type: typeof(ActionResult), description: "Bab Request")]
+        [SwaggerResponse(statusCode: 401, type: typeof(ActionResult), description: "Unauthorized")]
+        public abstract Task<IActionResult> AddTransaction([FromRoute][Required][RegularExpression("^(?<major>[0-9]+).(?<major>[0-9]+)$")] string version, [Required] Guid userId, [Required] Guid sessionId);
+
+        [HttpPut]
+        [EnableRateLimiting("IpPolicy")]
+        [Route("~/{version}/Transactions/")]
+        [SwaggerResponse(statusCode: 200, type: typeof(ActionResult), description: "Ok")]
+        [SwaggerResponse(statusCode: 400, type: typeof(ActionResult), description: "Bab Request")]
+        [SwaggerResponse(statusCode: 401, type: typeof(ActionResult), description: "Unauthorized")]
+        public abstract Task<IActionResult> UpdateTransaction([FromRoute][Required][RegularExpression("^(?<major>[0-9]+).(?<major>[0-9]+)$")] string version, [Required] Guid userId, [Required] Guid sessionId);
+
+        [HttpDelete]
+        [EnableRateLimiting("IpPolicy")]
+        [Route("~/{version}/Transactions/")]
+        [SwaggerResponse(statusCode: 200, type: typeof(ActionResult), description: "Ok")]
+        [SwaggerResponse(statusCode: 400, type: typeof(ActionResult), description: "Bab Request")]
+        [SwaggerResponse(statusCode: 401, type: typeof(ActionResult), description: "Unauthorized")]
+        public abstract Task<IActionResult> DeleteTransaction([FromRoute][Required][RegularExpression("^(?<major>[0-9]+).(?<major>[0-9]+)$")] string version, [Required] Guid userId, [Required] Guid sessionId);
+
+        [HttpGet]
+        [EnableRateLimiting("IpPolicy")]
+        [Route("~/{version}/Transactions/")]
+        [SwaggerResponse(statusCode: 200, type: typeof(ActionResult), description: "Ok")]
+        [SwaggerResponse(statusCode: 400, type: typeof(ActionResult), description: "Bab Request")]
+        [SwaggerResponse(statusCode: 401, type: typeof(ActionResult), description: "Unauthorized")]
+        public abstract Task<IActionResult> GetTransactions([FromRoute][Required][RegularExpression("^(?<major>[0-9]+).(?<major>[0-9]+)$")] string version, [Required] Guid userId, [Required] Guid sessionId);
+
+    }
+}
